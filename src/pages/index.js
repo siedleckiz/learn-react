c78486709fef0f04a5bf88ee4b82446eaef51a81
Zachary Siedlecki <siedlecki.z@northeastern.edu>
2024-02-16 16:59:08 -0500

    Changes made in class

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,12 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Thank from './qcomps/thankYouCrash'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Thank />
     </div>
   )
 }
